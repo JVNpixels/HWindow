ddea524feb46c9308b157cf2a08adc158154d679
JVN-Pixels <146671762+JVNpixels@users.noreply.github.com>
2025-10-16 23:43:27 -0700

    HWindow Class

---
+++ src/haxewindow/HWindow.hx
@@ -0,0 +1,40 @@
+package haxewindow;
+
+import lime.app.Application;
+import lime.graphics.Image;
+import openfl.Assets;
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+
+class HWindow
+{
+	public static function changeTitle(name:String)
+	{
+		Application.current.window.title = name;
+	}
+
+	public static function addToTitle(value:String)
+	{
+		Application.current.window.title += value;
+	}
+
+	public static function setIcon(path:String)
+	{
+		var image = Assets.getBytes(path);
+        var icon = Image.fromBytes(image);
+        Application.current.window.setIcon(icon);
+	}
+
+	public static function bounceWindow(direction:String, pixels:Int, comebackDur:Float)
+	{
+		switch (direction)
+		{
+			case "VERTICAL":
+				Application.current.window.x += pixels;
+				FlxTween.tween(Application.current.window, {x: Application.current.window.x - pixels}, comebackDur, {ease: FlxEase.quadInOut});
+			case "HORIZONTAL":
+				Application.current.window.y += pixels;
+				FlxTween.tween(Application.current.window, {y: Application.current.window.y - pixels}, comebackDur, {ease: FlxEase.quadInOut});
+		}
+	}
+}
